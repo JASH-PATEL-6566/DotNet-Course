1291ead34a088e3ebcff840d81dbc1323afb2540
JASH-PATEL-6566 <jashpatel6566@gmail.com>
2023-05-19 00:55:12 +0530

    end of basic dotnet,react,typescript topics

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // connection with database
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// this is a code by which you can set cors policy 
+// set cor policy which can allow localhost:3000 , allow all kind of header and method
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
 });
 
 // generating app object like express
@@ -24,6 +36,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// this line add cors policy, hence our browser allow to send request on this code
+// here bcz, it would be done before authentication and after app was created
+app.UseCors("CorsPolicy");
 
 // for check authantication purpose
 app.UseAuthorization();
@@ -48,7 +64,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occured during migration");
+    logger.LogError(ex, "An error occured during migration");
     throw;
 }
 
